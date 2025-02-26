457317cf376ffd753c7fb7af3b633cf3e84d07ab
KarinaSkirmante <karina.krinkele@gmail.com>
2025-02-26 09:54:42 +0200

    Edit: changed Student class with inheritance

--- src/model/Student.java
+++ src/model/Student.java
@@ -1,6 +1,6 @@
 package model;
 
-public class Student {
+public class Student extends Person {
 
 	//1. mainigie
 	private long stID;
@@ -12,56 +12,29 @@ public class Student {
 	public long getStID() {
 		return stID;
 	}
-	public String getName() {
-		return name;
-	}
-	public String getSurname() {
-		return surname;
-	}
+	
 
 	//3. setter
 	public void setStID() {
 		stID = counter++;
 	}
 	
-	public void setName(String inputName) {
-		if(inputName != null 
-				&& inputName.matches("[A-Z]{1}[a-z]{3,10}([ ][A-Z]{1}[a-z]{3,10})?"))
-		{
-			name = inputName;
-		}
-		else
-		{
-			name = "unknown";
-		}
-	}
-	
-	public void setSurname(String inputSurname) {
-		if(inputSurname != null
-				&& inputSurname.matches("[A-Z]{1}[a-z]{3,10}([-][A-Z]{1}[a-z]{3,10})?"))
-		{
-			surname = inputSurname;
-		}
-		else
-		{
-			surname = "unknown";
-		}
-	}
-	
 	
 	
 	//4. bez argumenta konstruktors
 	public Student() {
+		super();//tiek izsaukt bazes kalses bezragumenta konstruktors
 		setStID();
-		setName("Testa");
-		setSurname("Students");
+		//setSurname("Students");//set funkcija ir izsaukta no bazes klases
+		surname = "Students";
+		
 	}
 	
 	//5. argumena konstruktors
 	public Student(String name, String surname) {
+		super(name, surname);//tiek izsaukts bazes klases konstruktors Person(String inputName, String inputSurname)
 		setStID();
-		setName(name);
-		setSurname(surname);
+		
 	}
 
 	
@@ -70,7 +43,7 @@ public class Student {
 	//2: Karlis Lielais
 	@Override
 	public String toString() {
-		return stID + ": " + name + " " + surname;
+		return stID + ": " + super.toString();//pielipinas vardu un uzvardu no bazes klases toString funkcijas
 	}
 	
 	
